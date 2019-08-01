960759666f8fbc562fc862835d41e1334f57eef2
Camille TJHOA <camille.tjhoa@outlook.com>
2019-08-01 13:22:42 +0000

    Add AriaTab implementation

--- addon/components/aria-tab.js
+++ addon/components/aria-tab.js
@@ -1,6 +1,222 @@
 import Component from '@ember/component';
+import { computed, get, getProperties, setProperties } from '@ember/object';
+import { next } from '@ember/runloop';
 import layout from '../templates/components/aria-tab';
 
+const DEFAULT_CLASS = 'ember-tabs__tab';
+
+/**
+ * A tab component rendered as `<li />`.
+ *
+ * If you specify additional props on the `<AriaTab />` component they will be forwarded to the rendered `<li />`.
+ *
+ * @class AriaTab
+ * @public
+ */
 export default Component.extend({
-  layout
+  layout,
+  tagName: 'li',
+  classNames: [DEFAULT_CLASS],
+  classNameBindings: [
+    '_selectedClassName',
+    '_disabledClassName'
+  ],
+  attributeBindings: [
+    '_selected:aria-selected',
+    '_disabled:aria-disabled',
+    'disabled',
+    'panelId:aria-controls',
+    '_tabIndex:tabindex',
+    'parentGuid:data-parent-guid',
+    'role'
+  ],
+
+  /**
+   * Defaults to `false`.
+   *
+   * Disable this tab which will make it not do anything when clicked. Also a disabled class name will be added (see `disabledClassName`)
+   *
+   * @argument disabled
+   * @type Boolean
+   * @default false
+   */
+  disabled: false,
+
+  /**
+   * Defaults to `"ember-tabs__tab--disabled"`.
+   *
+   * Provide a custom class name for disabled tabs.
+   *
+   * > This option can also be set for all `<AriaTab />` components with the prop `disabledTabClassName` on `<AriaTabs />`.
+   *
+   * @argument disabledClassName
+   * @type String
+   * @default "ember-tabs__tab--disabled"
+   */
+  disabledClassName: null,
+
+  /**
+   * Defaults to `"ember-tabs__tab--selected"`.
+   *
+   * > This option can also be set for all `<AriaTab />` components with the prop `disabledTabClassName` on `<AriaTabs />`.
+   *
+   * @argument selectedClassName
+   * @type String
+   * @default "ember-tabs__tab--selected"
+   */
+  selectedClassName: null,
+
+  focus: false,
+
+  /**
+   * > default: if selected `"0"` otherwise `null`
+   *
+   * Overrides the tabIndex to enabled tabbing between tabs.
+   *
+   * @argument tabIndex
+   * @type String
+   * @default "0"|null
+   **/
+  tabIndex: null,
+
+  role: computed({
+    get() {
+      return 'tab';
+    }
+  }).readOnly(),
+
+  nodeIndex: computed('element', 'tabNodes.[]', function() {
+    let {
+      element,
+      tabNodes
+    } = getProperties(this, [
+      'element',
+      'tabNodes'
+    ]);
+    return tabNodes.indexOf(element);
+  }),
+
+  panelId: computed('nodeIndex', 'panelNodes.[]', function() {
+    let {
+      nodeIndex,
+      panelNodes
+    } = getProperties(this, [
+      'nodeIndex',
+      'panelNodes'
+    ]);
+    let panel = panelNodes[nodeIndex];
+    return panel? panel.id : null;
+  }),
+
+  selected: computed('nodeIndex', 'selectedIndex', function() {
+    let {
+      nodeIndex,
+      selectedIndex
+    } = getProperties(this, [
+      'nodeIndex',
+      'selectedIndex'
+    ]);
+    return nodeIndex === selectedIndex;
+  }),
+
+  _selected: computed('selected', function() {
+    return get(this, 'selected') ? 'true' : 'false';
+  }),
+
+  _selectedClassName: computed('selected', 'selectedTabClassName', 'selectedClassName', function() {
+    let {
+      selected,
+      selectedTabClassName,
+      selectedClassName
+    } = getProperties(this, [
+      'selected',
+      'selectedTabClassName',
+      'selectedClassName'
+    ]);
+    return selected ? (selectedClassName || selectedTabClassName || `${DEFAULT_CLASS}--selected`) : '';
+  }),
+
+  _disabled: computed('disabled', function() {
+    return get(this, 'disabled') ? 'true' : 'false';
+  }),
+
+  _disabledClassName: computed('disabled', 'disabledTabClassName', 'disabledClassName', function() {
+    let {
+      disabled,
+      disabledTabClassName,
+      disabledClassName
+    } = getProperties(this, [
+      'disabled',
+      'disabledTabClassName',
+      'disabledClassName'
+    ]);
+    return disabled ? (disabledClassName || disabledTabClassName || `${DEFAULT_CLASS}--disabled`) : '';
+  }),
+
+  _tabIndex: computed('tabIndex', 'selected', function() {
+    let {
+      tabIndex,
+      selected
+    } = getProperties(this, ['tabIndex', 'selected']);
+    return tabIndex || (selected ? '0' : null);
+  }),
+
+  init() {
+    this._super(...arguments);
+    // Set defaults
+    setProperties(this, {
+      tabNodes: [],
+      panelNodes: []
+    });
+  },
+
+  didInsertElement() {
+    this._super(...arguments);
+    this.checkFocus();
+  },
+
+  didUpdateAttrs() {
+    this._super(...arguments);
+    this.checkFocus();
+  },
+
+  click(e) {
+    let {
+      onClick,
+      nodeIndex
+    } = getProperties(this, [
+      'onClick',
+      'nodeIndex'
+    ]);
+    if (onClick) {
+      onClick(nodeIndex, e);
+    }
+  },
+
+  keyDown(e) {
+    let {
+      onKeyDown,
+      nodeIndex
+    } = getProperties(this, [
+      'onKeyDown',
+      'nodeIndex'
+    ]);
+    if (onKeyDown) {
+      onKeyDown(nodeIndex, e);
+    }
+  },
+
+  checkFocus() {
+    let {
+      element,
+      focus,
+      selected
+    } = getProperties(this, ['element', 'focus', 'selected']);
+    if (selected && focus) {
+      // We need to wait the selected rendering state
+      next(() => {
+        element.focus();
+      });
+    }
+  }
 });
--- tests/integration/components/aria-tab-test.js
+++ tests/integration/components/aria-tab-test.js
@@ -1,26 +1,77 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { render, setupOnerror } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
 module('Integration | Component | aria-tab', function(hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
-
+  test('it renders with sane default', async function(assert) {
     await render(hbs`<AriaTab />`);
 
-    assert.equal(this.element.textContent.trim(), '');
+    let tab = this.element.querySelector('[role="tab"]');
+    assert.equal(tab.textContent.trim(), '');
+  });
 
-    // Template block usage:
+  test('it renders with class', async function(assert) {
+    await render(hbs`<AriaTab class="foobar">Hello</AriaTab>`);
+
+    let tab = this.element.querySelector('[role="tab"]');
+    assert.equal(tab.classList.contains('foobar'), true);
+  });
+
+  test('it support being selected', async function(assert) {
     await render(hbs`
-      <AriaTab>
-        template block text
+      <AriaTab id="abcd" @selected={{true}} @panelId="1234">
+        Hello
       </AriaTab>
     `);
+    let tab = this.element.querySelector('[role="tab"]');
+    assert.equal(tab.id, 'abcd');
+    assert.equal(tab.textContent.trim(), 'Hello');
+    assert.equal(tab.getAttribute('aria-selected'), 'true');
+  });
 
-    assert.equal(this.element.textContent.trim(), 'template block text');
+  test('it support being selected with custom class name', async function(assert) {
+    await render(hbs`<AriaTab @selected={{true}} @selectedClassName="cool" />`);
+
+    let tab = this.element.querySelector('[role="tab"]');
+    assert.equal(tab.classList.contains('cool'), true);
+  });
+
+  test('it support being disabled', async function(assert) {
+    await render(hbs`<AriaTab @disabled={{true}} />`);
+
+    let tab = this.element.querySelector('[role="tab"]');
+    assert.equal(tab.getAttribute('aria-disabled'), 'true');
+  });
+
+  test('it support being disabled with custom class name', async function(assert) {
+    await render(hbs`<AriaTab @disabled={{true}} @disabledClassName="coolDisabled" />`);
+
+    let tab = this.element.querySelector('[role="tab"]');
+    assert.equal(tab.classList.contains('coolDisabled'), true);
+  });
+
+  test('it pass through custom properties', async function(assert) {
+    await render(hbs`<AriaTab data-tooltip="Tooltip contents" />`);
+
+    let tab = this.element.querySelector('[role="tab"]');
+    assert.equal(tab.getAttribute('data-tooltip'), 'Tooltip contents');
+  });
+
+  test('it does not allow overriding all default properties', async function(assert) {
+    setupOnerror(function(err) {
+      assert.ok(err);
+    });
+
+    await render(hbs`<AriaTab @role="micro-tab" />`);
+  });
+
+  test('it override the tabIndex if it was provided', async function(assert) {
+    await render(hbs`<AriaTab @tabIndex="0" />`);
+
+    let tab = this.element.querySelector('[role="tab"]');
+    assert.equal(tab.tabIndex, '0');
   });
 });
