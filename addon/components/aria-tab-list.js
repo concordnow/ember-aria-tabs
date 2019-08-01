53a2e3a679d3a6d1c8cdc85230a3a97292f35229
Camille TJHOA <camille.tjhoa@outlook.com>
2019-08-01 09:45:16 +0000

    Add dummy components

---
+++ addon/components/aria-tab-list.js
@@ -0,0 +1,6 @@
+import Component from '@ember/component';
+import layout from '../templates/components/aria-tab-list';
+
+export default Component.extend({
+  layout
+});
---
+++ app/components/aria-tab-list.js
@@ -0,0 +1,1 @@
+export { default } from 'ember-aria-tabs/components/aria-tab-list';
