256e6f044a11fd26e09c1e083edf9b9316b45ba6
Camille TJHOA <camille.tjhoa@outlook.com>
2019-08-01 14:24:19 +0000

    Add AriaTabPanel implementation

--- addon/components/aria-tab-panel.js
+++ addon/components/aria-tab-panel.js
@@ -1,6 +1,122 @@
 import Component from '@ember/component';
+import { computed, getProperties, setProperties } from '@ember/object';
 import layout from '../templates/components/aria-tab-panel';
 
+const DEFAULT_CLASS = 'ember-tabs__tab-panel';
+
+/**
+ * A tab panel component rendered as `<div />`.
+ *
+ * If you specify additional props on the `<AriaTabPanel />` component they will be forwarded to the rendered `<div />`.
+ *
+ * @class AriaTabPanel
+ * @public
+ */
 export default Component.extend({
-  layout
+  layout,
+  classNames: [DEFAULT_CLASS],
+  classNameBindings: [
+    '_selectedClassName',
+  ],
+  attributeBindings: [
+    'tabId:aria-labelledby',
+    'parentGuid:data-parent-guid',
+    'role'
+  ],
+
+  /**
+   * Defaults to `false`.
+   *
+   * By default the tab content will only be rendered when the tab is active. If set to `true` the tab will also be rendered if inactive.
+   *
+   * > This can also be enabled for all `<AriaTabPanel />` components with the prop `forceRenderTabPanel` on `<AriaTabs />`.
+   *
+   * @argument forceRender
+   * @type Boolean
+   * @default false
+   */
+  forceRender: false,
+
+  /**
+   * Defaults to `"ember-tabs__tab-panel--selected"`.
+   *
+   * Provide a custom class name for the active tab panel.
+   *
+   * > This option can also be set for all `<AriaTabPanel />` components with the prop `selectedTabPanelClassName` on `<AriaTabs />`.
+   *
+   * @argument selectedClassName
+   * @type String
+   * @default "ember-tabs__tab-panel--selected"
+   */
+  selectedClassName: null,
+
+  role: computed({
+    get() {
+      return 'tabpanel';
+    }
+  }).readOnly(),
+
+  nodeIndex: computed('element', 'panelNodes.[]', function() {
+    let {
+      element,
+      panelNodes
+    } = getProperties(this, [
+      'element',
+      'panelNodes'
+    ]);
+    return panelNodes.indexOf(element);
+  }),
+
+  tabId: computed('nodeIndex', 'tabNodes.[]', function() {
+    let {
+      nodeIndex,
+      tabNodes
+    } = getProperties(this, [
+      'nodeIndex',
+      'tabNodes'
+    ]);
+    let tab = tabNodes[nodeIndex];
+    return tab ? tab.id : null;
+  }),
+
+  selected: computed('nodeIndex', 'selectedIndex', function() {
+    let {
+      nodeIndex,
+      selectedIndex
+    } = getProperties(this, [
+      'nodeIndex',
+      'selectedIndex'
+    ]);
+    return nodeIndex === selectedIndex;
+  }),
+
+  _selectedClassName: computed('selected', 'selectedTabPanelClassName', 'selectedClassName', function() {
+    let {
+      selected,
+      selectedTabPanelClassName,
+      selectedClassName
+    } = getProperties(this, [
+      'selected',
+      'selectedTabPanelClassName',
+      'selectedClassName'
+    ]);
+    return selected ? (selectedClassName || selectedTabPanelClassName || `${DEFAULT_CLASS}--selected`) : '';
+  }),
+
+  shouldYield: computed('selected', 'forceRender', function() {
+    let {
+      selected,
+      forceRender
+    } = getProperties(this, ['selected', 'forceRender']);
+    return selected || forceRender;
+  }),
+
+  init() {
+    this._super(...arguments);
+    // Set defaults
+    setProperties(this, {
+      tabNodes: [],
+      panelNodes: []
+    });
+  }
 });
--- tests/integration/components/aria-tab-panel-test.js
+++ tests/integration/components/aria-tab-panel-test.js
@@ -1,26 +1,65 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { render, setupOnerror } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
 module('Integration | Component | aria-tab-panel', function(hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
-
-    await render(hbs`<AriaTabPanel />`);
+  test('it renders with sane defaults', async function(assert) {
+    await render(hbs`<AriaTabPanel>Hola</AriaTabPanel>`);
 
     assert.equal(this.element.textContent.trim(), '');
+  });
 
-    // Template block usage:
+  test('it renders when selected', async function(assert) {
+    await render(hbs`<AriaTabPanel @selected={{true}}>Hola</AriaTabPanel>`);
+
+    assert.equal(this.element.textContent.trim(), 'Hola');
+  });
+
+  test('it renders when forced', async function(assert) {
+    await render(hbs`<AriaTabPanel @forceRender={{true}}>Hola</AriaTabPanel>`);
+
+    assert.equal(this.element.textContent.trim(), 'Hola');
+  });
+
+  test('it renders with class', async function(assert) {
+    await render(hbs`<AriaTabPanel class="foobar" />`);
+
+    let tabPanel = this.element.querySelector('[role="tabpanel"]');
+    assert.equal(tabPanel.classList.contains('foobar'), true);
+  });
+
+  test('it does not allow overriding all default properties', async function(assert) {
+    setupOnerror(function(err) {
+      assert.ok(err);
+    });
+
+    await render(hbs`<AriaTabPanel @role="micro-tab" />`);
+  });
+
+  test('it supports being selected with custom class name', async function(assert) {
     await render(hbs`
-      <AriaTabPanel>
-        template block text
+      <AriaTabPanel
+        id="abcd"
+        @selected={{true}}
+        @tabId="1234"
+        @selectedClassName="selected"
+      >
+        Hola
       </AriaTabPanel>
-    `);
+  `);
 
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    let tabPanel = this.element.querySelector('[role="tabpanel"]');
+    assert.equal(tabPanel.getAttribute('aria-labelledby'), '1234');
+    assert.equal(tabPanel.classList.contains('selected'), true);
+  });
+
+  test('it should pass through custom properties', async function(assert) {
+    await render(hbs`<AriaTabPanel data-tooltip="Tooltip contents" />`);
+
+    let tab = this.element.querySelector('[role="tabpanel"]');
+    assert.equal(tab.getAttribute('data-tooltip'), 'Tooltip contents');
   });
 });
