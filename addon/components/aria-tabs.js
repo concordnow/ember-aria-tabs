7a32604bfd657970a4fb243b08ca220906410121
Camille TJHOA <camille.tjhoa@outlook.com>
2019-08-02 13:20:07 +0000

    Add AriaTabs implementation

--- addon/components/aria-tabs.js
+++ addon/components/aria-tabs.js
@@ -1,6 +1,370 @@
 import Component from '@ember/component';
+import { get, getProperties, set, setProperties } from '@ember/object';
+import { isNone } from '@ember/utils';
 import layout from '../templates/components/aria-tabs';
 
+const MODE_CONTROLLED = 0;
+const MODE_UNCONTROLLED = 1;
+
+const DEFAULT_CLASS = 'ember-tabs';
+
+/**
+ * The outer `<div />` of the tabs.
+ *
+ * If you specify additional props on the `<AriaTabs />` component they will be forwarded to the rendered `<div />`.
+ *
+ * @class AriaTabs
+ * @public
+ */
 export default Component.extend({
-  layout
+  layout,
+  classNames: [`${DEFAULT_CLASS}`],
+
+  /**
+   * Defaults to `false`.
+   *
+   * If set to `true` the tabs will be focused on initial render.
+   * This allows immediate use of keyboard keys to switch tabs after the first render.
+   *
+   * @argument defaultFocus
+   * @type Boolean
+   * @default true
+   */
+  defaultFocus: false,
+
+  /**
+   * Defaults to `0`.
+   *
+   * This allows changing the tab that should be open on initial render.
+   * This is a zero-based index, so first tab is `0`, second tab is `1`, ...
+   *
+   * > This can only be used in uncontrolled mode when react-tabs handles the current selected tab internally and for this reason cannot be used together with `selectedIndex`.
+   * > See `controlled vs uncontrolled mode` for more info on modes.
+   *
+   * @argument defaultIndex
+   * @type Number
+   * @default 0
+   */
+  defaultIndex: 0,
+
+  /**
+   * Defaults to `null`.
+   *
+   * This event handler is called every time a tab is about to change.
+   * It will be called with the `index` that it will be changed to, the `lastIndex` which was selected before and the underlying `event` which is usually either a `keydown` or `click` event.
+   * When `index` and `lastIndex` are equal it means the user clicked on the currently active tab.
+   *
+   * The callback can optionally return `false` to cancel the change to the new tab.
+   *
+   * > Returning `false` when the change to the new tab should be canceled is also important in controlled mode, as ember-tabs still internally handles the focus of the tabs.
+   *
+   * > In controlled mode the `onSelect` handler is a required prop.
+   *
+   * @argument onSelect
+   * @type Function
+   * @default null
+   */
+  onSelect: null,
+
+  /**
+   * Defaults to `null`.
+   *
+   * Set the currently selected tab. This is a zero-based index, so first tab is `0`, second tab is `1`, ...
+   *
+   * This enables controlled mode, which also requires `onSelect` to be set.
+   * See `controlled vs uncontrolled mode` for more info on modes.
+   *
+   * @argument selectedIndex
+   * @type Number
+   * @default null
+   */
+  selectedIndex: null,
+
+  /**
+   * Defaults to `"ember-tabs__tab--selected"`.
+   *
+   * Provide a custom class name for selected tabs.
+   *
+   * @argument selectedTabClassName
+   * @type String
+   * @default "ember-tabs__tab--selected"
+   */
+  selectedTabClassName: `${DEFAULT_CLASS}__tab--selected`,
+
+  /**
+   * Defaults to `"ember-tabs__tab-panel--selected"`.
+   *
+   * Provide a custom class name for selected panels.
+   *
+   * @argument selectedTabPanelClassName
+   * @type String
+   * @default "ember-tabs__tab-panel--selected"
+   */
+  selectedTabPanelClassName: `${DEFAULT_CLASS}__tab-panel--selected`,
+
+  /**
+    * Defaults to `"ember-tabs__tab--disabled"`.
+    *
+    * Provide a custom class name for disabled tabs.
+    *
+    * @argument disabledTabClassName
+    * @type String
+    * @default "ember-tabs__tab--disabled"
+    */
+  disabledTabClassName: `${DEFAULT_CLASS}__tab--disabled`,
+
+  /**
+   * Defaults to `false`.
+   *
+   * By default only the current active tab will be rendered to DOM.
+   * If set to `true` all tabs will be rendered to the DOM always.
+   *
+   * > This can also be enabled for each individual `<AriaTabPanel />` component with its prop `forceRender`.
+   *
+   * @argument forceRenderTabPanel
+   * @type Boolean
+   * @default false
+   */
+  forceRenderTabPanel: false,
+
+  init() {
+    this._super(...arguments);
+
+    setProperties(this, {
+      tabNodes: [],
+      panelNodes: [],
+      focus: get(this, 'defaultFocus')
+    });
+  },
+
+  didReceiveAttrs() {
+    this._super(...arguments);
+
+    let mode = get(this, 'mode');
+    let newMode = this.getMode();
+
+    if (!isNone(mode) && mode !== newMode) {
+      throw new Error(
+        `Switching between controlled mode (by using \`selectedIndex\`) and uncontrolled mode is not supported in \`AriaTabs\`.
+For more information about controlled and uncontrolled mode of ember-tabs see the README.`,
+      );
+    }
+
+    set(this, 'mode', newMode);
+  },
+
+  getMode() {
+    return get(this, 'selectedIndex') === null ? MODE_UNCONTROLLED : MODE_CONTROLLED;
+  },
+
+  didRender() {
+    this._super(...arguments);
+
+    let {
+      element,
+      elementId,
+      defaultIndex,
+      mode,
+      selectedIndex
+    } = getProperties(this, [
+      'element',
+      'elementId',
+      'defaultIndex',
+      'mode',
+      'selectedIndex'
+    ]);
+    let tabNodes = Array.from(element.querySelectorAll(`[role="tab"][data-parent-guid="${elementId}"]`));
+    let panelNodes = Array.from(element.querySelectorAll(`[role="tabpanel"][data-parent-guid="${elementId}"]`));
+
+    // Avoid infinite loop
+    if (!this.nodesEquals('tabNodes', tabNodes) && !this.nodesEquals('panelNodes', panelNodes)) {
+      setProperties(this, {
+        tabNodes,
+        panelNodes
+      });
+    }
+
+    if (mode === MODE_UNCONTROLLED) {
+      const maxTabIndex = tabNodes.length - 1;
+      let newSelectedIndex = null;
+
+      if (selectedIndex != null) {
+        newSelectedIndex = Math.min(selectedIndex, maxTabIndex);
+      } else {
+        newSelectedIndex = defaultIndex || 0;
+      }
+      set(this, 'selectedIndex', newSelectedIndex);
+    }
+  },
+
+  nodesEquals(propName, nodeList) {
+    let previousNodes = get(this, propName);
+    if (previousNodes.length !== nodeList.length) {
+      return false;
+    }
+    let nodeIds = nodeList.map((node) => node.id);
+    let previousNodeIds = previousNodes.map((node) => node.id);
+    return previousNodeIds.every((prevNodeId) => nodeIds.includes(prevNodeId));
+  },
+
+  setSelected(index, event) {
+    // Check index boundary
+    if (index < 0 || index >= get(this, 'tabsNodes.length')) {
+      return;
+    }
+
+    let {
+      onSelect,
+      selectedIndex: last,
+      mode
+    } = getProperties(this, [
+      'onSelect',
+      'selectedIndex',
+      'mode'
+    ]);
+    // Call change event handler
+    if (typeof onSelect === 'function') {
+      if (onSelect(index, last, event) === false) {
+        // Check if the change event handler cancels the tab change
+        return;
+      }
+    }
+
+    set(this, 'focus', event.type === 'keydown');
+
+    if (mode === MODE_UNCONTROLLED) {
+      set(this, 'selectedIndex', index);
+    }
+  },
+
+  getNextTab(index) {
+    let tabsNodes = get(this, 'tabNodes');
+    const count = tabsNodes.length;
+
+    // Look for non-disabled tab from index to the last tab on the right
+    for (let i = index + 1; i < count; i++) {
+      if (!this.isTabDisabled(tabsNodes[i])) {
+        return i;
+      }
+    }
+
+    // If no tab found, continue searching from first on left to index
+    for (let i = 0; i < index; i++) {
+      if (!this.isTabDisabled(tabsNodes[i])) {
+        return i;
+      }
+    }
+
+    // No tabs are disabled, return index
+    return index;
+  },
+
+  getPrevTab(index) {
+    let tabsNodes = get(this, 'tabNodes');
+    let i = index;
+
+    // Look for non-disabled tab from index to first tab on the left
+    while (i--) {
+      if (!this.isTabDisabled(tabsNodes[i])) {
+        return i;
+      }
+    }
+
+    // If no tab found, continue searching from last tab on right to index
+    i = tabsNodes.length;
+    while (i-- > index) {
+      if (!this.isTabDisabled(tabsNodes[i])) {
+        return i;
+      }
+    }
+
+    // No tabs are disabled, return index
+    return index;
+  },
+
+  getFirstTab() {
+    let tabsNodes = get(this, 'tabNodes');
+    const count = tabsNodes.length;
+
+    // Look for non disabled tab from the first tab
+    for (let i = 0; i < count; i++) {
+      if (!this.isTabDisabled(tabsNodes[i])) {
+        return i;
+      }
+    }
+
+    return null;
+  },
+
+  getLastTab() {
+    let tabsNodes = get(this, 'tabNodes');
+    let i = tabsNodes.length;
+
+    // Look for non disabled tab from the last tab
+    while (i--) {
+      if (!this.isTabDisabled(tabsNodes[i])) {
+        return i;
+      }
+    }
+
+    return null;
+  },
+
+  isTabDisabled(tabNode) {
+    return tabNode.getAttribute('aria-disabled') === 'true';
+  },
+
+  actions: {
+    handleClick(index, e) {
+      let tabsNodes = get(this, 'tabNodes');
+      if (this.isTabDisabled(tabsNodes[index])) {
+        return;
+      }
+      this.setSelected(index, e);
+    },
+
+    handleKeyDown(index, e) {
+      let preventDefault = false;
+      let useSelectedIndex = false;
+
+      if (e.keyCode === 32 || e.keyCode === 13) {
+        preventDefault = true;
+        useSelectedIndex = false;
+        this.setSelected(index, e);
+      }
+
+      if (e.keyCode === 37 || e.keyCode === 38) {
+        // Select next tab to the left
+        index = this.getPrevTab(index);
+        preventDefault = true;
+        useSelectedIndex = true;
+      } else if (e.keyCode === 39 || e.keyCode === 40) {
+        // Select next tab to the right
+        index = this.getNextTab(index);
+        preventDefault = true;
+        useSelectedIndex = true;
+      } else if (e.keyCode === 35) {
+        // Select last tab (End key)
+        index = this.getLastTab();
+        preventDefault = true;
+        useSelectedIndex = true;
+      } else if (e.keyCode === 36) {
+        // Select first tab (Home key)
+        index = this.getFirstTab();
+        preventDefault = true;
+        useSelectedIndex = true;
+      }
+
+      // This prevents scrollbars from moving around
+      if (preventDefault) {
+        e.preventDefault();
+        e.stopPropagation();
+      }
+
+      // Only use the selected index in the state if we're not using the tabbed index
+      if (useSelectedIndex) {
+        this.setSelected(index, e);
+      }
+    }
+  }
 });
--- tests/integration/components/aria-tabs-test.js
+++ tests/integration/components/aria-tabs-test.js
@@ -1,26 +1,492 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { findAll, click, render, focus, triggerKeyEvent } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
+
+function createTabs() {
+  return render(hbs`
+      <AriaTabs as |at|>
+        <at.tabList as |tl|>
+          <tl.tab>Foo</tl.tab>
+          <tl.tab>Bar</tl.tab>
+          <tl.tab>
+            <a href="a">Baz</a>
+          </tl.tab>
+          <tl.tab @disabled={{true}}>Qux</tl.tab>
+        </at.tabList>
+        <at.tabPanel>Hello Foo</at.tabPanel>
+        <at.tabPanel>Hello Bar</at.tabPanel>
+        <at.tabPanel>Hello Baz</at.tabPanel>
+        <at.tabPanel>Hello Qux</at.tabPanel>
+      </AriaTabs>
+    `);
+}
+
+function assertTabSelected(assert, index) {
+  const tab = findAll('[role="tab"]')[index];
+  const panel = findAll('[role="tabpanel"]')[index];
+
+  assert.equal(tab.getAttribute('aria-selected'), 'true');
+  assert.notEqual(panel.textContent, '');
+}
 
 module('Integration | Component | aria-tabs', function(hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
+  test('it renders with sane default', async function(assert) {
+    await createTabs();
 
-    await render(hbs`<AriaTabs />`);
+    let tabList = findAll('[role="tablist"]');
+    let tabs = findAll('[role="tab"]');
+    let tabPanels = findAll('[role="tabpanel"]');
+    let disabledTab = findAll('[role="tab"][aria-disabled="true"]');
+    assert.equal(tabList.length, 1);
+    assert.equal(tabs.length, 4);
+    assert.equal(tabPanels.length, 4);
+    assert.equal(disabledTab.length, 1);
+    assert.equal(disabledTab[0].textContent.trim(), 'Qux');
 
-    assert.equal(this.element.textContent.trim(), '');
+    assert.equal(tabPanels[0].textContent.trim(), 'Hello Foo');
+    assert.equal(tabPanels[1].textContent.trim(), '');
+    assert.equal(tabPanels[2].textContent.trim(), '');
+    assert.equal(tabPanels[3].textContent.trim(), '');
+  }),
 
-    // Template block usage:
+  test('it renders with positive defaultIndex prop', async function(assert) {
     await render(hbs`
-      <AriaTabs>
-        template block text
+      <AriaTabs @defaultIndex={{1}} as |at|>
+        <at.tabList as |tl|>
+          <tl.tab>Foo</tl.tab>
+          <tl.tab>Bar</tl.tab>
+          <tl.tab>
+            <a href="a">Baz</a>
+          </tl.tab>
+          <tl.tab @disabled={{true}}>Qux</tl.tab>
+        </at.tabList>
+        <at.tabPanel>Hello Foo</at.tabPanel>
+        <at.tabPanel>Hello Bar</at.tabPanel>
+        <at.tabPanel>Hello Baz</at.tabPanel>
+        <at.tabPanel>Hello Qux</at.tabPanel>
       </AriaTabs>
     `);
 
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    let selectedTab = findAll('[role="tab"][aria-selected="true"]');
+    let tabPanels = findAll('[role="tabpanel"]');
+
+    assert.equal(selectedTab.length, 1);
+    assert.equal(selectedTab[0].textContent, 'Bar');
+    assert.equal(tabPanels[0].textContent.trim(), '');
+    assert.equal(tabPanels[1].textContent.trim(), 'Hello Bar');
+    assert.equal(tabPanels[2].textContent.trim(), '');
+    assert.equal(tabPanels[3].textContent.trim(), '');
+  });
+
+  test('it renders with negative defaultIndex prop', async function(assert) {
+    await render(hbs`
+      <AriaTabs @defaultIndex={{-1}} as |at|>
+        <at.tabList as |tl|>
+          <tl.tab>Foo</tl.tab>
+          <tl.tab>Bar</tl.tab>
+          <tl.tab>
+            <a href="a">Baz</a>
+          </tl.tab>
+          <tl.tab @disabled={{true}}>Qux</tl.tab>
+        </at.tabList>
+        <at.tabPanel>Hello Foo</at.tabPanel>
+        <at.tabPanel>Hello Bar</at.tabPanel>
+        <at.tabPanel>Hello Baz</at.tabPanel>
+        <at.tabPanel>Hello Qux</at.tabPanel>
+      </AriaTabs>
+    `);
+
+    let selectedTab = findAll('[role="tab"][aria-selected="true"]');
+    let tabPanels = findAll('[role="tabpanel"]');
+
+    assert.equal(selectedTab.length, 0);
+
+    assert.equal(tabPanels[0].textContent.trim(), '');
+    assert.equal(tabPanels[1].textContent.trim(), '');
+    assert.equal(tabPanels[2].textContent.trim(), '');
+    assert.equal(tabPanels[3].textContent.trim(), '');
+  });
+
+  test('it calls onSelect when selection changes', async function(assert) {
+    let called = { index: -1, last: -1 };
+    this.set('onSelect', (index, last) => {
+      called.index = index;
+      called.last = last;
+    });
+
+    await render(hbs`
+      <AriaTabs @onSelect={{action onSelect}} as |at|>
+        <at.tabList as |tl|>
+          <tl.tab>Foo</tl.tab>
+          <tl.tab>Bar</tl.tab>
+          <tl.tab>
+            <a href="a">Baz</a>
+          </tl.tab>
+          <tl.tab @disabled={{true}}>Qux</tl.tab>
+        </at.tabList>
+        <at.tabPanel>Hello Foo</at.tabPanel>
+        <at.tabPanel>Hello Bar</at.tabPanel>
+        <at.tabPanel>Hello Baz</at.tabPanel>
+        <at.tabPanel>Hello Qux</at.tabPanel>
+      </AriaTabs>
+    `);
+
+    await click(findAll('[role="tab"]')[1]);
+
+    assert.equal(called.index, 1);
+    assert.equal(called.last, 0);
+  });
+
+  test('it renders with class names', async function(assert) {
+    await render(hbs`
+      <AriaTabs class="foobar" as |at|>
+        <at.tabList as |tl|>
+          <tl.tab>Foo</tl.tab>
+          <tl.tab>Bar</tl.tab>
+          <tl.tab>
+            <a href="a">Baz</a>
+          </tl.tab>
+          <tl.tab @disabled={{true}}>Qux</tl.tab>
+        </at.tabList>
+        <at.tabPanel>Hello Foo</at.tabPanel>
+        <at.tabPanel>Hello Bar</at.tabPanel>
+        <at.tabPanel>Hello Baz</at.tabPanel>
+        <at.tabPanel>Hello Qux</at.tabPanel>
+      </AriaTabs>
+    `);
+
+    assert.equal(this.element.children[0].classList.contains('foobar'), true);
+  });
+
+
+  test('it updates selectedIndex when clicked', async function(assert) {
+    await createTabs();
+
+    await click(findAll('[role="tab"]')[1]);
+
+    assertTabSelected(assert, 1);
+  });
+
+  test('it updates selectedIndex when tab child is clicked', async function(assert) {
+    await render(hbs`
+      <AriaTabs as |at|>
+        <at.tabList as |tl|>
+          <tl.tab>Foo</tl.tab>
+          <tl.tab>Bar</tl.tab>
+          <tl.tab>
+            <button class="foobar">Baz</button>
+          </tl.tab>
+          <tl.tab @disabled={{true}}>Qux</tl.tab>
+        </at.tabList>
+        <at.tabPanel>Hello Foo</at.tabPanel>
+        <at.tabPanel>Hello Bar</at.tabPanel>
+        <at.tabPanel>Hello Baz</at.tabPanel>
+        <at.tabPanel>Hello Qux</at.tabPanel>
+      </AriaTabs>
+    `);
+
+    await click('.foobar');
+
+    assertTabSelected(assert, 2);
+  });
+
+  test('it does not change selectedIndex when clicking a disabled tab', async function(assert) {
+    await createTabs();
+
+    await click(findAll('[role="tab"]')[3]);
+
+    assertTabSelected(assert, 0);
+  });
+
+  test('it updates selectedIndex when arrow right key pressed', async function(assert) {
+    await createTabs();
+
+    let target = findAll('[role="tab"]')[1];
+
+    await click(target);
+    await focus(target);
+    await triggerKeyEvent(target, 'keydown', 'ArrowRight');
+
+    assertTabSelected(assert, 2);
+  });
+
+  test('it does not not change selectedIndex when arrow left key pressed on a disabled tab', async function(assert) {
+    await createTabs();
+
+    let target = findAll('[role="tab"]')[0];
+
+    await click(target);
+    await focus(target);
+    await triggerKeyEvent(target, 'keydown', 'ArrowLeft');
+
+    assertTabSelected(assert, 2);
+  });
+
+  test('it only renders the selected tab', async function(assert) {
+    await createTabs();
+
+    let tabPanels = findAll('[role="tabpanel"]');
+    assert.equal(tabPanels[0].textContent.trim(), 'Hello Foo');
+    assert.equal(tabPanels[1].textContent.trim(), '');
+    assert.equal(tabPanels[2].textContent.trim(), '');
+
+    await click(findAll('[role="tab"]')[1]);
+
+    assert.equal(tabPanels[0].textContent.trim(), '');
+    assert.equal(tabPanels[1].textContent.trim(), 'Hello Bar');
+    assert.equal(tabPanels[2].textContent.trim(), '');
+
+    await click(findAll('[role="tab"]')[2]);
+
+    assert.equal(tabPanels[0].textContent.trim(), '');
+    assert.equal(tabPanels[1].textContent.trim(), '');
+    assert.equal(tabPanels[2].textContent.trim(), 'Hello Baz');
+  });
+
+  test('it renders all tabs if forceRenderTabPanel is true', async function(assert) {
+    await render(hbs`
+      <AriaTabs @forceRenderTabPanel={{true}} as |at|>
+        <at.tabList as |tl|>
+          <tl.tab>Foo</tl.tab>
+          <tl.tab>Bar</tl.tab>
+          <tl.tab>
+            <a href="a">Baz</a>
+          </tl.tab>
+          <tl.tab @disabled={{true}}>Qux</tl.tab>
+        </at.tabList>
+        <at.tabPanel>Hello Foo</at.tabPanel>
+        <at.tabPanel>Hello Bar</at.tabPanel>
+        <at.tabPanel>Hello Baz</at.tabPanel>
+        <at.tabPanel>Hello Qux</at.tabPanel>
+      </AriaTabs>
+    `);
+
+    let tabPanels = findAll('[role="tabpanel"]');
+    assert.equal(tabPanels[0].textContent.trim(), 'Hello Foo');
+    assert.equal(tabPanels[1].textContent.trim(), 'Hello Bar');
+    assert.equal(tabPanels[2].textContent.trim(), 'Hello Baz');
+    assert.equal(tabPanels[3].textContent.trim(), 'Hello Qux');
+  });
+
+  test('it renders all tabs if forceRenderTabPanel is true', async function(assert) {
+    await render(hbs`
+      <AriaTabs @forceRenderTabPanel={{true}} as |at|>
+        <at.tabList as |tl|>
+          <tl.tab>Foo</tl.tab>
+          <tl.tab>Bar</tl.tab>
+          <tl.tab>
+            <a href="a">Baz</a>
+          </tl.tab>
+          <tl.tab @disabled={{true}}>Qux</tl.tab>
+        </at.tabList>
+        <at.tabPanel>Hello Foo</at.tabPanel>
+        <at.tabPanel>Hello Bar</at.tabPanel>
+        <at.tabPanel>Hello Baz</at.tabPanel>
+        <at.tabPanel>Hello Qux</at.tabPanel>
+      </AriaTabs>
+    `);
+
+    let tabPanels = findAll('[role="tabpanel"]');
+    assert.equal(tabPanels[0].textContent.trim(), 'Hello Foo');
+    assert.equal(tabPanels[1].textContent.trim(), 'Hello Bar');
+    assert.equal(tabPanels[2].textContent.trim(), 'Hello Baz');
+    assert.equal(tabPanels[3].textContent.trim(), 'Hello Qux');
+  });
+
+  test('it renders without children', async function(assert) {
+    await render(hbs`<AriaTabs />`);
+    assert.equal(this.element.textContent.trim(), '');
+  });
+
+  test('it renders with just tabList', async function(assert) {
+    await render(hbs`
+      <AriaTabs as |at|>
+        <at.tabList />
+      </AriaTabs>
+    `);
+    assert.equal(this.element.textContent.trim(), '');
+  });
+
+  test('it renders conditionally', async function(assert) {
+    await render(hbs`
+      <AriaTabs as |at|>
+        <at.tabList as |tl|>
+          <tl.tab>Foo</tl.tab>
+          {{#if false}}
+            <tl.tab>Bar</tl.tab>
+          {{/if}}
+        </at.tabList>
+        <at.tabPanel>Hello Foo</at.tabPanel>
+        {{#if false}}
+          <at.tabPanel>Hello Bar</at.tabPanel>
+        {{/if}}
+      </AriaTabs>
+    `);
+
+    let tabs = findAll('[role="tab"]');
+    let tabPanels = findAll('[role="tabpanel"]');
+    assert.equal(tabs.length, 1);
+    assert.equal(tabPanels.length, 1);
+  });
+
+  test('it supports nested tabs', async function(assert) {
+    await render(hbs`
+      <AriaTabs class="first" as |at|>
+        <at.tabList as |tl|>
+          <tl.tab>Foo</tl.tab>
+          <tl.tab>Bar</tl.tab>
+        </at.tabList>
+        <at.tabPanel>
+          <AriaTabs class="second" as |at2|>
+            <at2.tabList as |tl2|>
+              <tl2.tab />
+              <tl2.tab />
+            </at2.tabList>
+            <at2.tabPanel />
+            <at2.tabPanel />
+          </AriaTabs>
+        </at.tabPanel>
+        <at.tabPanel />
+      </AriaTabs>
+    `);
+
+    await click(findAll('.second > [role="tablist"] > [role="tab"]')[1]);
+
+    const tab = findAll('.first > [role="tablist"] > [role="tab"]')[0];
+    assert.equal(tab.getAttribute('aria-selected'), 'true');
+
+    const tab2 = findAll('.second > [role="tablist"] > [role="tab"]')[1];
+    assert.equal(tab2.getAttribute('aria-selected'), 'true');
+  });
+
+  test('it allows other DOM nodes', async function(assert) {
+    await render(hbs`
+      <AriaTabs as |at|>
+        <div id="tabs-nav-wrapper">
+          <button type="button">Left</button>
+          <div class="tabs-container">
+            <at.tabList as |tl|>
+              <tl.tab />
+              <tl.tab />
+            </at.tabList>
+          </div>
+          <button type="button">Right</button>
+        </div>
+        <div class="tab-panels">
+          <at.tabPanel />
+          <at.tabPanel />
+        </div>
+      </AriaTabs>
+    `);
+
+    const buttons = findAll('#tabs-nav-wrapper button');
+    assert.equal(buttons.length, 2);
+    assert.equal(buttons[0].textContent, 'Left');
+    assert.equal(buttons[1].textContent, 'Right');
+
+    let tabList = findAll('.tabs-container [role="tablist"]');
+    let tabs = findAll('.tabs-container [role="tab"]');
+    let tabPanels = findAll('.tab-panels [role="tabpanel"]');
+    assert.equal(tabList.length, 1);
+    assert.equal(tabs.length, 2);
+    assert.equal(tabPanels.length, 2);
+  });
+
+  test('it cancels if event handler returns false', async function(assert) {
+    this.set('onSelect', () => false);
+
+    await render(hbs`
+      <AriaTabs @onSelect={{action onSelect}} as |at|>
+        <at.tabList as |tl|>
+          <tl.tab>Foo</tl.tab>
+          <tl.tab>Bar</tl.tab>
+          <tl.tab>
+            <a href="a">Baz</a>
+          </tl.tab>
+          <tl.tab @disabled={{true}}>Qux</tl.tab>
+        </at.tabList>
+        <at.tabPanel>Hello Foo</at.tabPanel>
+        <at.tabPanel>Hello Bar</at.tabPanel>
+        <at.tabPanel>Hello Baz</at.tabPanel>
+        <at.tabPanel>Hello Qux</at.tabPanel>
+      </AriaTabs>
+    `);
+
+    assertTabSelected(assert, 0);
+
+    await click(findAll('[role="tab"]')[1]);
+
+    assertTabSelected(assert, 0);
+
+    await click(findAll('[role="tab"]')[2]);
+
+    assertTabSelected(assert, 0);
+  });
+
+  test('it trigger onSelect handler when clicking', async function(assert) {
+    let wasClicked = false;
+    this.set('onSelect', () => {
+      wasClicked = true;
+    });
+
+    await render(hbs`
+      <AriaTabs @onSelect={{action onSelect}} as |at|>
+        <at.tabList as |tl|>
+          <tl.tab>Foo</tl.tab>
+          <tl.tab>Bar</tl.tab>
+          <tl.tab>
+            <a href="a">Baz</a>
+          </tl.tab>
+          <tl.tab @disabled={{true}}>Qux</tl.tab>
+        </at.tabList>
+        <at.tabPanel>Hello Foo</at.tabPanel>
+        <at.tabPanel>Hello Bar</at.tabPanel>
+        <at.tabPanel>Hello Baz</at.tabPanel>
+        <at.tabPanel>Hello Qux</at.tabPanel>
+      </AriaTabs>
+    `);
+
+    assertTabSelected(assert, 0);
+
+    await click(findAll('[role="tab"]')[1]);
+
+    assertTabSelected(assert, 1);
+
+    assert.equal(wasClicked, true);
+  });
+
+  test('it trigger onSelect handler when clicking on open tab', async function(assert) {
+    let wasClicked = false;
+    this.set('onSelect', () => {
+      wasClicked = true;
+    });
+
+    await render(hbs`
+      <AriaTabs @onSelect={{action onSelect}} as |at|>
+        <at.tabList as |tl|>
+          <tl.tab>Foo</tl.tab>
+          <tl.tab>Bar</tl.tab>
+          <tl.tab>
+            <a href="a">Baz</a>
+          </tl.tab>
+          <tl.tab @disabled={{true}}>Qux</tl.tab>
+        </at.tabList>
+        <at.tabPanel>Hello Foo</at.tabPanel>
+        <at.tabPanel>Hello Bar</at.tabPanel>
+        <at.tabPanel>Hello Baz</at.tabPanel>
+        <at.tabPanel>Hello Qux</at.tabPanel>
+      </AriaTabs>
+    `);
+
+    assertTabSelected(assert, 0);
+
+    await click(findAll('[role="tab"]')[0]);
+
+    assertTabSelected(assert, 0);
+
+    assert.equal(wasClicked, true);
   });
 });
